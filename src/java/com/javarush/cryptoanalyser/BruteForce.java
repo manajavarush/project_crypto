7cfae6d199951a09f28c30a07c76afbae86545bd
Stanislau Sinkevich <manamlmain@gmail.com>
2022-03-24 15:41:28 +0300

    Created executable jar, changed Brute Force method to avoid conflict with file

--- src/java/com/javarush/cryptoanalyser/BruteForce.java
+++ src/java/com/javarush/cryptoanalyser/BruteForce.java
@@ -29,10 +29,27 @@ public class BruteForce {
         String secondOption = null;
         String completeFailure = "Sorry, but we can't decoding this text";
 
+/*      Так не работает, проблема с путями к файлу. После сборки исполняемого jar мой файл words находит
+        только по абсолютному пути(такое решение будет работать только под Windows)
 
         String textFromFile = readString("src/java/com/javarush/cryptoanalyser/words.txt");
         String[] arrayOfWords = textFromFile.split(",");
         Set<String> words = new HashSet<>(List.of(arrayOfWords));
+*/
+
+        Set<String> words = new HashSet<>(Arrays.asList("with", "the", "be", "of", "and", "a","in", "to", "have", "it", "for","that", "you",
+                "he", "on", "than","do", "at", "by", "not", "this","but", "from", "they", "his", "she","or", "which",
+                "as", "we", "an","say", "will", "would", "can", "if","their", "go", "want", "there", "what","all", "too",
+                "get", "her", "make", "who","out", "up", "see", "no", "time", "them", "some", "could", "so", "him", "large",
+                "year", "into", "its", "then", "think", "my", "come", "more", "about", "now", "know", "last", "your",
+                "me", "other", "give", "just", "should", "these", "those", "people", "two", "one", "three", "also",
+                "either", "etc", "well", "only", "any", "new", "very", "when", "why", "may", "way", "look", "like", "use",
+                "such", "how", "because", "good", "find", "man", "woman", "our", "you know", "must", "I", "us", "day",
+                "off", "thing", "body", "still", "back", "five", "six", "seven", "eight", "nine", "ten", "life", "case",
+                "great", "own", "self", "world", "feel", "again", "yes", "number", "web", "part", "Internet", "week",
+                "bring", "begin", "always", "member", "house", "country", "city", "town", "person", "course", "really",
+                "under", "shall", "leave", "most", "proud", "system", "keep", "fit", "mean", "group", "problem", "follow",
+                "set", "every", "end", "state", "right", "left", "social", "mind", "while", "during", "whole", "become"));
 
         while (key < S_LENGTH - 1) {
             String decodingText = Decoding.decode(cryptoText, key);
